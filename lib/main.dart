21e4317286452e5cff8e0a13ad4647dd283a6350
11mrhappy <nekomamiremy@gmail.com>
2021-08-21 13:08:35 +0900

    get error when import package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +9,16 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+          )
         ),
       ),
     );
   }
 }
+
+// get error
+//The current Dart SDK version is 2.10.5.
+//
+// Because xylophone depends on audioplayers >=0.18.0 which requires SDK version >=2.12.0 <3.0.0, version solving failed.
+// pub get failed (1; Because xylophone depends on audioplayers >=0.18.0 which requires SDK version >=2.12.0 <3.0.0, version solving failed.)
